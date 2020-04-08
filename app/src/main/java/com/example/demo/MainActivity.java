0ce76e9ae5245323a878967ecb5eca60ac3f04a5
Shail089 <verma.shailendra725@gmail.com>
2020-04-08 21:38:10 +0530

    github copmare added

--- app/src/main/java/com/example/demo/MainActivity.java
+++ app/src/main/java/com/example/demo/MainActivity.java
@@ -12,14 +12,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     private void testFuntion(){
-        int i=1+1;
-        int j=2+2;
+        int i=10;
+        int j=40;
         int k=i+j;
     }
 
     public void newMethod(){
         int a=2;
-        String test="Merging";
+        String test="Complex Merging ";
         String test2="compare on github";
     }
 }
